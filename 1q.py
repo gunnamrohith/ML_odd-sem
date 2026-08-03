7b9cd3927c8ec263304474c2ec1ef973a5e7be76
gunnamrohith <gunnamrohith@gmail.com>
2026-08-03 14:46:32 +0530

    Delete 1q.py

--- 1q.py
+++
@@ -1,9 +0,0 @@
-# Prompt the user to enter two numbers
-num1 = input("Enter first number: ")
-num2 = input("Enter second number: ")
-
-# Convert the string inputs into floating-point numbers and add them
-sum_result = float(num1) + float(num2)
-
-# Display the final sum
-print(f"The sum of {num1} and {num2} is {sum_result}")
